259d6fe27d237a4bfcf926e81e78d710cade1218
Justin Clark <jclarksummit@gmail.com>
2023-04-24 18:25:21 -0400

    Add routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import DogFinder from "./DogFinder"
 
 function App() {
   return (
     <div className="App">
-      <h1>Good luck!</h1>
+      <h1>Dog Finder</h1>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<DogFinder />} />
+          <Route path="/dogs/:name" element={<DogFinder />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
