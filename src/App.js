1972d0e35b6a71ebde410856380a1baeb1c5405d
Thomas Wilson <tommyhwilson3@gmail.com>
2023-04-24 18:22:04 -0500

    broke some stuff

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { BrowserRouter, Routes, Route } from "react-router-dom"
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom"
 import DogFinder from "./DogFinder"
 
 function App() {
@@ -8,8 +8,9 @@ function App() {
       <h1>Dog Finder</h1>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<DogFinder />} />
+          <Route path="/dogs" element={<DogFinder />} />
           <Route path="/dogs/:name" element={<DogFinder />} />
+          <Route path="*" element={<Navigate to='/dogs' />} />
         </Routes>
       </BrowserRouter>
     </div>
--- src/DogFinder.js
+++ src/DogFinder.js
@@ -2,21 +2,27 @@ import React, { useState } from "react";
 import { useParams } from "react-router";
 import axios from "axios";
 
-const BASE_URL = "http://localhost:5001"
+const BASE_URL = "http://localhost:5001";
 
 function DogFinder() {
+  const [dogData, setdogData] = useState([]);
+  const [loading, setLoading] = useState(false);
 
   const { name } = useParams();
 
   async function getAllDogs() {
-    const dogList = await axios.get(`${BASE_URL}/dogs`)
-    return dogList.data.map(d => d = {name: d.name, age: d.age, image: d.src, facts: d.facts})
-  }
+    setLoading(true);
+    const dogList = await axios.get(`${BASE_URL}/dogs`);
 
-  const dogList = getAllDogs()
+    // dogList.data.map(
+    //   (d) => (d = { name: d.name, age: d.age, image: d.src, facts: d.facts }));
+    setdogData(dogList.data)
+    setLoading(false);
+  };
+  const dogList = getAllDogs();
 
-  console.log(dogList)
 
+  console.log(dogList);
 
   if (name) {
     // Check if params exist
@@ -25,13 +31,15 @@ function DogFinder() {
     // Render DogDetails
   }
 
-
   // If no params
   // Get all dog data
   // Pass as params into DogList and render
 
-
-
+  return (
+    <div>
+      {loading ? <h4>loading..</h4> : <dogList />}
+    </div>
+  )
 }
 
 export default DogFinder;
